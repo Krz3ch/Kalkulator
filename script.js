3af147d6f01642b17b4bd01ec99e1d09f4c98563
Krzysztof Gadzina <krzysiek.gadzina123@gmail.com>
2023-09-11 20:03:17 +0200

    dodanie zmiennych

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    aktualna_liczba = document.querySelector(".dodawana");
+
+    poprzednia_liczba = document.querySelector(".poprzednia");
+
+    znak = document.querySelector(".znak");
+
+    clear_btn = document.querySelector(".clear");
+
+    operator = document.querySelectorAll(".operator");
+
+    numer = document.querySelectorAll(".numer");
+
+    wynik = document.querySelector(".rownasie")
+
+
+
+    
+
+
+
+})
